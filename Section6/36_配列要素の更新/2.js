81c05580192414dda935a75edc2f759896a69dd7
Soh1121 <satou.shg@gmail.com>
2022-01-09 19:11:35 +0900

    [add]配列要素の更新方法を学習

---
+++ Section6/36_配列要素の更新/2.js
@@ -0,0 +1,18 @@
+db.users.update(
+    {
+        name: "田中 剛"
+    },
+    {
+        $set: {
+            "bookshelf.$[item].status": "読了2",
+            "bookshelf.$[item].readed": ISODate()
+        }
+    },
+    {
+        arrayFilters: [
+            {
+                "item.book": ObjectId("5b6f033a6c25c37f77e6917e")
+            }
+        ]
+    }
+)
