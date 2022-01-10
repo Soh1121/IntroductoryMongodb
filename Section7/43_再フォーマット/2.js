ca727561c069bff5ca6dfb58e8d08d067f0361d6
Soh1121 <satou.shg@gmail.com>
2022-01-10 15:00:27 +0900

    [add]再フォーマット方法を学習

---
+++ Section7/43_再フォーマット/2.js
@@ -0,0 +1,15 @@
+db.books.aggregate(
+    [
+        {
+            $match: {
+                publisher: "講話社"
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                title: 1
+            }
+        }
+    ]
+)
