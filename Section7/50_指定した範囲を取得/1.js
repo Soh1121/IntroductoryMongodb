b28af1c5d579c98cde00ef689e8cc8c27c564632
Soh1121 <satou.shg@gmail.com>
2022-01-10 16:49:53 +0900

    [add]指定した範囲を取得する方法を学習

---
+++ Section7/50_指定した範囲を取得/1.js
@@ -0,0 +1,16 @@
+db.books.aggregate(
+    [
+        {
+            $project: {
+                _id: 0,
+                title: 1,
+                launch: 1
+            }
+        },
+        {
+            $sort: {
+                launch: -1
+            }
+        }
+    ]
+)
---
+++ Section7/50_指定した範囲を取得/3.js
@@ -0,0 +1,22 @@
+db.books.aggregate(
+    [
+        {
+            $project: {
+                _id: 0,
+                title: 1,
+                launch: 1
+            }
+        },
+        {
+            $sort: {
+                launch: -1
+            }
+        },
+        {
+            $skip: 2
+        },
+        {
+            $limit: 3
+        }
+    ]
+)
